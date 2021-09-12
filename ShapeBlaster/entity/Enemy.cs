0ca0e1deb428a8d5a94a1b51c662f79dd0177f6a
Stuart <stuart.haas2@gmail.com>
2021-09-12 08:31:00 -0400

    Organizing classes

---
+++ ShapeBlaster/entity/Enemy.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ShapeBlaster
+{
+    class Enemy: Entity
+    {
+        private int timeUntilStart = 60;
+        public bool isActive {  get { return timeUntilStart <= 0;  } }
+
+        public Enemy(Texture2D image, Vector2 position)
+        {
+            this.image = image;
+            Position = position;
+            Radius = image.Width / 2f;
+            color = Color.Transparent;
+        }
+
+        public override void Update()
+        {
+            if(timeUntilStart <= 0)
+            {
+                // enemy behavior
+            }
+            else
+            {
+                timeUntilStart--;
+                color = Color.White * (1 - timeUntilStart / 60f);
+            }
+
+            Position += Velocity;
+            Position = Vector2.Clamp(Position, Size / 2, GameRoot.ScreenSize - Size / 2);
+
+            Velocity *= 0.8f;
+        }
+
+        public void WasShot()
+        {
+            IsExpired = true;
+        }
+    }
+}
